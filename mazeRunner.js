5b45704099caa5be8bac5a81662d6084b58b61c7
yassinechih7 <72637629+yassinechih7@users.noreply.github.com>
2021-09-07 15:50:59 +0100

    Update mazeRunner.js

--- mazeRunner.js
+++ mazeRunner.js
@@ -1,11 +1,12 @@
-// N = Up // E = Right // W = Left // S = Down
 
 function mazeRunner(maze, directions) {
+  const [SAFE_PLACE_TO_WALK, WALL, START_POINT, FINISH_POINT] = [0, 1, 2, 3];
+
   let [currentLocation, wayOut] = [
     () => {
       let pos = [];
       maze.forEach((e, index) => {
-        const elementPostion = e.indexOf(2);
+        const elementPostion = e.indexOf(START_POINT);
         if (elementPostion != -1) {
           pos = [index, elementPostion];
         }
@@ -15,7 +16,7 @@ function mazeRunner(maze, directions) {
     () => {
       let pos = [];
       maze.forEach((e, index) => {
-        const elementPostion = e.indexOf(3);
+        const elementPostion = e.indexOf(FINISH_POINT);
         if (elementPostion != -1) {
           pos = [index, elementPostion];
         }
@@ -25,6 +26,7 @@ function mazeRunner(maze, directions) {
   ];
 
   const moves = {
+    // N = Up // E = Right // W = Left // S = Down
     N: () => {},
     E: () => {},
     W: () => {},
